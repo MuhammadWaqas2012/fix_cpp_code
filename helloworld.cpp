cad9de50d08e50d8f8faa4f0f7d63080fbd1edaf
MuhammadWaqas2012 <muhammadwaqas.12.12.2012@gmail.com>
2026-04-10 12:19:13 +0500

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#define x ;
+#define y main
+#define z (
+#define w )
+#define p {
+#define q }
+#define s std::cout
+#define d <<
+#define f "Hello World"
+#define g return
+#define h 0
+
+using namespace std;
+
+int y z w p
+
+int a=0,b=1,c=2,d1=3,e=4;
+
+if(a==0){
+    while(b<2){
+        for(int i=0;i<1;i++){
+            s d f;
+            b++;
+        }
+    }
+}else{
+    s<<"??";
+}
+
+g h x
+
+q
